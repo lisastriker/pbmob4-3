cfdabe6c9575690848c9feb9da2017891776c10c
lisastriker <lisastriker@gmail.com>
2020-12-17 14:49:18 +0800

    added navigators

---
+++ screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+export default function LoginScreen(){
+    return(
+    <View>
+        <Text>Login Screen</Text>
+    </View>
+    );
+}
